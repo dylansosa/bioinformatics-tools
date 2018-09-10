b050a392cab7489abc6fd77393196fd0376d748f
Dylan <savetrees@ymail.com>
2018-09-10 13:11:23 -0500

    Delete data2csv.py

--- data2csv.py
+++
@@ -1,9 +0,0 @@
-import os, re
-
-# usage:
-# python convert2csv.py > [output.csv]
-with open('RHS.PID.toxins.domainarch', 'r') as data:
-    for line in data:
-        row = re.split(r'\s{2,}', line) # splitting if separated by >=2 whitespaces
-        print row
-# this output then needs to be opened in excel. Find and replace unnecessary characters.
