185a4a534c1187084255ef50718c443df30372d4
Dylan <savetrees@ymail.com>
2019-08-05 05:59:24 -0500

    Update condenseTranscriptsAndExpression.sh

--- condenseTranscriptsAndExpression.sh
+++ condenseTranscriptsAndExpression.sh
@@ -7,10 +7,10 @@
 header="$(cat GSE99574_HiSAT2_dmel.transcript_level_TPM.FB.txt | head -n1)"
 
 # if transcript expression values correspond to the same gene, sum and collapse into one row
-for ((i = 4; i <=64; i++)); do awk -v column=$i '{ seen[$2] += $column } END { for (j in seen) print j, seen[j] }' test_Dmel_expression_noHeader | sort -k1 > Dmel_expression$i ; done
+for ((i = 4; i <=131; i++)); do awk -v column=$i '{ seen[$2] += $column } END { for (j in seen) print j, seen[j] }' test_Dmel_expression_noHeader | sort -k1 > Dmel_expression$i ; done
 
 # gets just expression value column and saves in new files
-for ((k = 4; k <=64; k++)); do awk '{print $2}' Dmel_expression$k > expression$k ; done
+for ((k = 4; k <=131; k++)); do awk '{print $2}' Dmel_expression$k > expression$k ; done
 for n in $(seq 4 9); do mv expression$n expression0$n ; done;
 
 # add expression columns to corresponding genes
